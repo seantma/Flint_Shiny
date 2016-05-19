242c3d9cc66f5981a649f2d9f711570036ed3484
Sean Ma <tehsheng@gmail.com>
2016-05-19 18:16:24 -0400

    changing bpp sliding scale increment

--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@ ui <- bootstrapPage(
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
                 sliderInput("range", "Lead Level", min(res_sent$Lead..ppb.), max(res_sent$Lead..ppb.),
-                            value = range(res_sent$Lead..ppb.), step = 5),
+                            value = range(res_sent$Lead..ppb.), step = 15),
                 # selectInput("colors", "Color Scheme",
                 #             rownames(subset(brewer.pal.info, category %in% c("seq", "div")))),
                 checkboxInput("legend", "Show legend", TRUE)
